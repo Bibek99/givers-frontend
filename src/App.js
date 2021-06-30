25527e311e2227ffa1c471f742d922d1c384bcea
Bibek Basyal <bibekbasyal22@gmail.com>
2021-06-30 16:54:39 +0545

    small adjustment in styling of landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import GetStartedPage from './pages/GetStartedPage';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
@@ -9,12 +9,14 @@ import Page404 from './pages/Page404';
 function App() {
     return (
         <Router>
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/signup" component={GetStartedPage} />
-            <Route path="/user" component={UserDashboard} />
-            <Route path="/org" component={OrgDashboard} />
-            <Route component={Page404} />
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route path="/login" component={LoginPage} />
+                <Route path="/signup" component={GetStartedPage} />
+                <Route path="/user" component={UserDashboard} />
+                <Route path="/org" component={OrgDashboard} />
+                <Route path="*" component={Page404} />
+            </Switch>
         </Router>
     );
 }
--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -5,20 +5,20 @@ import { ArrowDownIcon } from '@heroicons/react/solid';
 const HeorSection = () => {
     return (
         <div className="">
-            <div className="flex flex-col text-center justify-center mt-40">
+            <div className="flex flex-col text-center justify-center mt-48">
                 <div className="relative w-full max-w-screen-lg mx-auto">
                     <div className="animate-blob absolute w-72 h-72 -left-4 -top-16 bg-purple-300 rounded-full filter blur-2xl opacity-90"></div>
                     <div className="animate-blob2 absolute w-36 h-36 right-8 top-48 bg-orange-300 rounded-full filter blur-2xl opacity-90"></div>
-                    <div className="relative space-y-8">
-                        <div className="text-4xl md:text-6xl font-semibold text-center px-4">
+                    <div className="relative">
+                        <div className="text-4xl md:text-6xl font-semibold text-center px-4 mb-8">
                             Create Impact with larger group of manpower
                         </div>
-                        <div className="text-2xl md:text-3xl text-center text-gray-500 px-4">
+                        <div className="text-2xl md:text-3xl text-center text-gray-500 px-4 mb-12 sm:mb-8 md:mb-4">
                             Get going with the industry standard tool for
                             managing your event volunteers and create the right
                             impact towards giving to the society.
                         </div>
-                        <div className="flex-auto lg:flex-1 space-y-6 space-x-8 text-center px-4">
+                        <div className="lg:flex-1 space-x-8 text-center px-4">
                             <Link
                                 to="/signup"
                                 className="bg-purple-500 py-3 px-12 text-xl text-white rounded-lg"
@@ -27,7 +27,7 @@ const HeorSection = () => {
                             </Link>
                             <Link
                                 to="#"
-                                className="py-3 px-12 text-xl text-purple-500 rounded-lg inline-flex"
+                                className="py-3 px-12 text-xl text-purple-500 rounded-lg inline-flex mt-8"
                             >
                                 See Features
                                 <span>
