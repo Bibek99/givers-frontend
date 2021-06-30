a0c70a010f13d18d32d623df663d699bb972ffeb
Bibek Basyal <bibekbasyal22@gmail.com>
2021-06-30 12:42:52 +0545

    mistakenly deleted landing page restored

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainNav from '../components/navs/MainNav';
+import HeroSection from '../components/sections/HeroSection';
+
+const LandingPage = () => {
+    return (
+        <>
+            <MainNav />
+            <HeroSection />
+        </>
+    );
+};
+
+export default LandingPage;
