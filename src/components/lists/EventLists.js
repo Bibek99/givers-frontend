2502f77f335362d23015bde8bb1b602caaef813c
Bibek Basyal <bibekbasyal22@gmail.com>
2021-07-10 12:46:14 +0545

    event loading

--- src/components/lists/EventLists.js
+++ src/components/lists/EventLists.js
@@ -13,27 +13,22 @@ const EventLists = () => {
 
     useEffect(() => {
         dispatch(loadEvents(access));
-        console.log('use effect fired');
     }, [dispatch, access]);
 
     const { loading, eventsList } = useSelector((state) => state.events);
 
     return (
-        // <div>
-        //     {loading ? (
-        //         <div>Event Loading</div>
-        //     ) : (
-        //         eventsList.map((event, index) => {
-        //             return (
-        //                 <React.Fragment key={index}>
-        //                     <Event event={event} />
-        //                 </React.Fragment>
-        //             );
-        //         })
-        //     )}
-        // </div>
         <div>
-            <EventLoading />
+            {loading && [1, 2, 3].map((n) => <EventLoading key={n} />)}
+
+            {eventsList &&
+                eventsList.map((event, index) => {
+                    return (
+                        <React.Fragment key={index}>
+                            <Event event={event} />
+                        </React.Fragment>
+                    );
+                })}
         </div>
     );
 };
