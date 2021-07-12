2b83233a0938a10b39d1ab7584a429851dd3cd6f
Bibek Basyal <bibekbasyal22@gmail.com>
2021-07-12 10:06:59 +0545

    strips moved to a component and hidden on small screen sizes

---
+++ src/components/sections/Stirps.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ReactComponent as RightBand1 } from '../../assets/right-band-1.svg';
+import { ReactComponent as RightBand2 } from '../../assets/right-band-2.svg';
+import { ReactComponent as RightBand3 } from '../../assets/right-band-3.svg';
+import { ReactComponent as LeftBand1 } from '../../assets/left-band-1.svg';
+import { ReactComponent as LeftBand2 } from '../../assets/left-band-2.svg';
+
+const Stirps = () => {
+    return (
+        <div className="hidden md:block relative -mt-8">
+            <div className="absolute top-0 -right-96 md:-right-64 lg:-right-44 xl:-right-16">
+                <RightBand1 />
+            </div>
+            <div className="absolute top-15 -right-96 md:-right-64 lg:-right-44 xl:-right-16">
+                <RightBand2 />
+            </div>
+            <div className="absolute top-30 -right-96 md:-right-64 lg:-right-44 xl:-right-16">
+                <RightBand3 />
+            </div>
+            <div className="absolute top-38 md:top-44 lg:top-52 xl:top-60 -left-48 lg:-left-16 xl:left-0">
+                <LeftBand1 />
+            </div>
+            <div className="absolute top-52 md:top-58 lg:top-68 xl:top-76 -left-32 lg:-left-16 xl:left-0">
+                <LeftBand2 />
+            </div>
+        </div>
+    );
+};
+
+export default Stirps;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import MainNav from '../components/navs/MainNav';
 import HeroSection from '../components/sections/HeroSection';
-import { ReactComponent as RightBand1 } from '../assets/right-band-1.svg';
-import { ReactComponent as RightBand2 } from '../assets/right-band-2.svg';
-import { ReactComponent as RightBand3 } from '../assets/right-band-3.svg';
-import { ReactComponent as LeftBand1 } from '../assets/left-band-1.svg';
-import { ReactComponent as LeftBand2 } from '../assets/left-band-2.svg';
+import Stirps from '../components/sections/Stirps';
 
 const LandingPage = () => {
     return (
@@ -13,23 +9,7 @@ const LandingPage = () => {
             <MainNav />
             <HeroSection />
 
-            <div className="relative -mt-8">
-                <div className="absolute top-0 -right-96 md:-right-64 lg:-right-44 xl:-right-16">
-                    <RightBand1 />
-                </div>
-                <div className="absolute top-15 -right-96 md:-right-64 lg:-right-44 xl:-right-16">
-                    <RightBand2 />
-                </div>
-                <div className="absolute top-30 -right-96 md:-right-64 lg:-right-44 xl:-right-16">
-                    <RightBand3 />
-                </div>
-                <div className="absolute top-38 md:top-44 lg:top-52 xl:top-60 -left-48 lg:-left-16 xl:left-0">
-                    <LeftBand1 />
-                </div>
-                <div className="absolute top-52 md:top-58 lg:top-68 xl:top-76 -left-32 lg:-left-16 xl:left-0">
-                    <LeftBand2 />
-                </div>
-            </div>
+            <Stirps />
         </div>
     );
 };
