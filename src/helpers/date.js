6a38be9fcf56927b4d134e472e09a0db9ffaa543
Bibek Basyal <bibekbasyal22@gmail.com>
2021-07-15 12:25:01 +0545

    posted date format change

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -16,12 +16,42 @@ export const formatDate = (date) => {
 
     var dateArr = date.split('-');
 
-    const newDate =
-        months[parseInt(dateArr[1])] +
-        ' ' +
-        dateArr[2].substring(0, 2) +
-        ', ' +
-        dateArr[0];
+    let newDate = '';
+
+    if (dateArr[2].length > 2) {
+        let dayArr = dateArr[2].split('T');
+        let timeArr = dayArr[1].split('.');
+        let hr = timeArr[0].substring(0, 2);
+        let min = timeArr[0].substring(3, 5);
+        let timeDay = '';
+
+        if (hr >= 12) {
+            hr = hr - 12;
+            timeDay = 'PM';
+        } else {
+            timeDay = 'AM';
+        }
+
+        newDate =
+            months[parseInt(dateArr[1])] +
+            ' ' +
+            dateArr[2].substring(0, 2) +
+            ', ' +
+            dateArr[0] +
+            ' ' +
+            hr +
+            ':' +
+            min +
+            ' ' +
+            timeDay;
+    } else {
+        newDate =
+            months[parseInt(dateArr[1])] +
+            ' ' +
+            dateArr[2].substring(0, 2) +
+            ', ' +
+            dateArr[0];
+    }
 
     return newDate;
 };
